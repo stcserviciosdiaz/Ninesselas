7d45bfdffb05ffa4b671ce3d1813fd33a736722d
Danni Brito <daniebro.5@gmail.com>
2019-11-15 10:45:53 -0500

    Intento1

---
+++ NinesselasBackend/api/models/Contratado.js
@@ -0,0 +1,17 @@
+/**
+ * Contratado.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+
+
+  },
+
+};
+
--- NinesselasBackend/config/datastores.js
+++ NinesselasBackend/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://root:Daniebro1995@host:3307/Ninesselas',
 
   },
 
