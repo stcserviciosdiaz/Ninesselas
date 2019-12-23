2af960ff006755f421237a6daa67f45212181c57
Luis Diaz <stcserviciosdiaz@outlook.com>
2019-12-23 13:01:16 -0500

    working

--- FrontView/src/app/components/user/forgot/forgot.component.ts
+++ FrontView/src/app/components/user/forgot/forgot.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.component.css']
 })
 export class ForgotComponent implements OnInit {
-
-  constructor() { }
+  ResetForm: FormGroup;
+  submitted = false;
+  @ViewChild (TemplateRef, {static: false}) tpl: TemplateRef <any>;
+  constructor(
+    private authService: AuthService, 
+    private FormBuilder: FormBuilder,
+    public ngxSmartModalService: NgxSmartModalService,
+    private router: Router,
+  ) { 
+    this.ResetRegisterForm();
+  }
 
   ngOnInit() {
+  }
+
+  ResetRegisterForm(){
+    this.ResetForm = this.FormBuilder.group({
+      email: [null, [Validators.required, Validators.email]]
+    })
+  }
+
+  sendPasswordResetRequest(){
+    const email = this.ResetForm.controls['email'].value;
   }
 
 }
