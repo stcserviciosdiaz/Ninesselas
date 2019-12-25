636b35b75b3db60343205230d37d2ee0d924e342
Willys <orochi_cris@yahoo.com>
2019-12-25 17:08:57 -0500

    se a;ade funcionalidad para compa;ias

--- FrontView/src/app/components/user/register/register.component.ts
+++ FrontView/src/app/components/user/register/register.component.ts
@@ -73,18 +73,112 @@ export class RegisterComponent implements OnInit {
     });
   }
 
+  
+
   registrarEmpresa() {
     this.submitted = true;
     // stop the process here if form is invalid
-    if (this.companyForm.invalid) {
+    /*if (this.companyForm.invalid) {
       return;
-    }
+    }*/
+
     const newUserObject = this.companyForm.value;
+
+
+    const usuario = {
+      "acento": newUserObject.acento,
+      "altura": newUserObject.altura,
+      "apellidos": newUserObject.apellidos,
+      "carnetConducir": newUserObject.carnetConducir,
+      "codigoPostal": newUserObject.codpostal,
+      "colorOjos": newUserObject.colorOjos,
+      "colorPelo": newUserObject.colorPelo,
+      "colorPiel": newUserObject.colorPiel,
+      "curriculumVitae": '',
+      "direccion": newUserObject.direccion,
+      "dniMadre": '',
+      "dniRepresentante": '',
+      "dniPadre": '',
+      "dniUser": newUserObject.numeroDNI,
+      "email": newUserObject.email,
+      "fechaNacimiento": newUserObject.fechaNacimiento,
+      "libroFamilia": '',
+      "localidad": newUserObject.localidad,
+      "nacionalidad": newUserObject.nacionalidad,
+      "nombreArtistico": newUserObject.nombreArtistico,
+      "nombreCompleto": '',
+      "nombres": newUserObject.nombres,
+      "numeroSeguroSocial": newUserObject.numeroSeguridadSocial,
+      "observaciones": newUserObject.observaciones,
+      "password": newUserObject.password,
+      "pathDniMadre": '',
+      "pathDniPadre": '',
+      "pathDniUser": '',
+      "pathDniRepresentante": '',
+      "pathSeguroSocial": '',
+      "provincia": newUserObject.provincia,
+      "sexo": newUserObject.sexo,
+      "telefono": newUserObject.telefono,
+      "telefonoMadre": '',
+      "telefonoPadre": '',
+      "lugarNacimiento": newUserObject.lugarNacimiento,
+      "edad": 0,
+      "actor": '',
+      "username": newUserObject.username,
+      "videobook": newUserObject.videoBook,
+      "instrumentoList": [],
+      "estilosCantoList": [],
+      "deporteList": [
+        {
+          "idDeporte": 1,
+          "descripcionDeporte": "NO APLICA",
+          "nombreDeporte": "NO APLICA"
+        }
+      ],
+      "estiloBaileList": [],
+      "idiomasList": [],
+      "habilidadesList": [],
+      "tallasList": [],
+      "ultimosTrabajosList": [],
+      "idCantante": {
+        "idCantante": 1,
+        "descripcionCantante": "NO APLICA",
+        "nombreCantante": "NOAPLICA"
+      },
+      "idBailarin": {
+        "idBailarin": 1,
+        "descripcionBailarin": "NO APLICA",
+        "nombreBailarin": "NO APLICA"
+      },
+      "idEtnia": {
+        "idEtnia": 1,
+        "nombreEtnia": "BLANCO editado"
+      },
+      "idType": {
+        "idType": 4,
+        "description": "COMPAÑIA",
+        "nombres": "COMPAÑIA"
+      },
+      "idDeportista": {
+        "idDeportista": 1,
+        "descripcionDeportista": "NO APLICA",
+        "nombreDeportista": "NO APLICA"
+      },
+      "idMusico": {
+        "idMusico": 1,
+        "descipcionMusico": "NO APLICA",
+        "nombreMusico": "NO APLICA"
+      },
+      "motoList": [],
+      "cocheList": [],
+      "fotosTatuajesList": [],
+      "fotosManosList": []
+    }
     newUserObject.rol = 'Company';
-    this.authService.signup(newUserObject)
+    this.authService.signup2(usuario)
       .subscribe(
        res => {
-         localStorage.setItem('token', res.token);
+         localStorage.setItem('token', res.idUser);
          this.ngxSmartModalService.create('confirm', 'Cuenta de Companía creada exitosamente').open();
          this.router.navigate(['/company']);
          },
