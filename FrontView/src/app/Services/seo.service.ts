82c06f2ee9f0191a4165dd3677485aa3d1103af1
Luis Diaz <stcserviciosdiaz@outlook.com>
2020-03-10 18:13:08 -0500

    update admin empresa

--- FrontView/src/app/Services/seo.service.ts
+++ FrontView/src/app/Services/seo.service.ts
@@ -28,6 +28,6 @@ export class SeoService {
     this.meta.updateTag({ property: 'og:title', content: config.title });
     this.meta.updateTag({ property: 'og:description', content: config.description });
     this.meta.updateTag({ property: 'og:image', content: config.image });
-    this.meta.updateTag({ property: 'og:url', content: `localhost/${config.slug}` });
+    this.meta.updateTag({ property: 'og:url', content: `https://www.ninesselas.com/${config.slug}` });
   }
 }
